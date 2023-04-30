9ccdf464c019b2968435bbc0119a25471dc1b508
mirajbhetwal <128876088+mirajbhetwal@users.noreply.github.com>
2023-04-30 17:02:36 +1000

    Add files via upload

---
+++ ClientSocket.cs
@@ -0,0 +1,27 @@
+﻿/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Student name:   Miraj Bhetwal
+/// Student ID:     A00105794
+/// Assessment_2:   Networking Project
+////////////////////////////////////////////
+/////////////////////////////////////////// 
+
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Windows_Forms_Chat
+{
+    public class ClientSocket
+    {
+        //add other attributes to this, e.g username, what state the client is in etc
+        public Socket socket;
+        public string username;
+        public const int BUFFER_SIZE = 2048;
+        public byte[] buffer = new byte[BUFFER_SIZE];
+        public bool is_connected = false;
+        public bool is_mod = false;
+        public bool show_timestamps = false;
+    }
+}
